a1f17bbdc59362622c6f5f8186801d794d8fcc5e
MarkosyanShooshan <markosyanshooshan@gmail.com>
2026-06-24 15:25:10 +0400

    Refactor input handling and error messages

--- calc.try_except.py
+++ calc.try_except.py
@@ -1,49 +1,52 @@
 def number():
     while True:
         try:
-            number = float(input('Enter a number:'))
-            return number
+            num = float(input('Enter a number: '))
+            return num
         except ValueError:
             print('Only the entered number is accepted!')
 
 
 def choice():
-    ch = ('+','-', '/', '*')
-    c = input("Choose any arithmetic operation:'+', '-', '/', '*'")
-    try:
-        if c not in ch:
-            raise Exception
-    except:
-        print("only", ch)
-        c = input("'+', '-', '/', '*'") 
+    ch = ('+', '-', '/', '*')
+    while True:
+        c = input("Choose any arithmetic operation: '+', '-', '/', '*' : ")
+        try:
+            if c not in ch:
+                raise ValueError  
+            return c
+        except ValueError:
+            print("Only these operations are allowed:", ch)
 
-    return c 
 
 def result():
     x = number()
     c = choice()
-    y= number()
+    y = number()
+    
     if c == '+':
         res = x + y
         return f"{x} + {y} = {res}"
     elif c == '-':
-        res = x-y
+        res = x - y
         return f"{x} - {y} = {res}"
     elif c == '*':
-        res = x*y
-        return f"{x} * {y}={res}"
+        res = x * y
+        return f"{x} * {y} = {res}"
     elif c == '/':
         while True:
             try:
-                if y==0:
-                    raise Exception
-                res= x/y
-                return f" {x}/{y}={res}"
-            except Exception:
+                if y == 0:
+                    raise ZeroDivisionError  
+                res = x / y
+                return f"{x} / {y} = {res}"
+            except ZeroDivisionError:
                 print("A number is not divisible by 0!")
-                y=number()
-               
-print(result())                
+                y = number()
 
+               
+print(result())
+    
+   
 
     
